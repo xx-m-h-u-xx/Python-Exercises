ee064ccc791c7ae261fb76510f59a770dbf94654
xx-m-h-u-xx <56305452+xx-m-h-u-xx@users.noreply.github.com>
2021-03-02 13:39:40 +0000

    Add files via upload

---
+++ Challenge 6 MK 2.py
@@ -0,0 +1,16 @@
+#Challenge - Minutes in a Week generator
+
+import time
+
+dpw=7
+#Days Per Week
+hpd=24
+#Hours Per Day
+mph=60
+#Minutes Per Hour
+print("Minutes In A Week")
+print(dpw*hpd*mph)
+#Days Per Week*Hours Per Day*Minutes Per Hour
+time.sleep(2)
+
+
