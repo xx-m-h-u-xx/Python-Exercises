af33de4d7ec612f8297fd15bb1cd0589438de6d9
xx-m-h-u-xx <56305452+xx-m-h-u-xx@users.noreply.github.com>
2021-03-02 13:28:30 +0000

    Add files via upload

---
+++ Cover - Days Alives Calculator.py
@@ -0,0 +1,35 @@
+seconds_in_a_month = 86400
+day = 30
+print("The number of seconds in a 30 day month = ", seconds_in_a_month * day)
+
+
+seconds_in_a_year = 2592000
+year = 1
+print("The number of seconds in a year is", seconds_in_a_year * year)
+
+
+answer = 9 * 9
+print(answer)
+
+
+time = 414 / 150
+print("The time it will take", time, "minutes")
+
+
+answer = 49 / 7
+print("the answer is", answer)
+
+
+answer = 8 ** 2
+print("the answer is", answer)
+
+
+answer = 20 % 3
+print("the answer is", answer)
+
+
+answer = 17 // 3
+print("the answer is", answer)
+
+
+
