dbf8acd534ead6be8ef54e146a1c6d28a20858b3
xx-m-h-u-xx <56305452+xx-m-h-u-xx@users.noreply.github.com>
2021-03-02 13:22:31 +0000

    Add files via upload

---
+++ Python-Maths-Prgrm-Quiz.py
@@ -0,0 +1,35 @@
+score = 0
+print("What is 2 + 2?")
+answer = input()
+answer = int(answer)
+if answer == 4:
+    print("Well done")
+    score=score+1
+else:
+    print("Sorry the answer was 4")
+print("score is", score)
+print("What is 6 / 3?")
+answer = input()
+answer = int(answer)
+if answer == 2:
+    print("Its Correct!")
+    score=score+2
+else:
+    print("Sorry the answer was 2")
+print("score is", score")
+print("What is half of 4")
+answer = input()
+answer = int(answer)
+if answer == 2:
+    print("Its Too Easy For You!")
+else:
+    print("Too Bad!!!",score)
+print("What is 6 * 6 - 6")
+answer = input()
+answer = int(answer)
+if answer == 30:
+    print("OMG!")
+else:
+    print("Unlucky")
+answer = input ()
+answer = int(answer)
